5baa3f10ecbef30bb33c05e52b524973467ff463
Livia John <liviajohn1@gmail.com>
2024-03-05 15:24:17 -0500

    Delete Lab 6 FibFormula.java

--- Lab 6 FibFormula.java
+++
@@ -1,8 +0,0 @@
-class FibFormula implements FindFib {
-    @Override
-    public int calculate_fib(int n) {
-        double sqrt5 = Math.sqrt(5);
-        double phi = (1 + sqrt5) / 2;
-        return (int) Math.round((Math.pow(phi, n) - Math.pow(1 - phi, n)) / sqrt5);
-    }
-}
